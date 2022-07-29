7af9822725d64c04f2a048b09772d7f833cbb8ff
Shubham Choudhari <schoudhari@platform9.com>
2022-07-29 12:42:45 +0530

    :sparkles: Added Installtion script

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # dotfiles
 collection of my dotfile
+
+
+
+## Installation
+
+```bash
+sh -c "$(curl -fsSL https://github.com/joey00072/dotfiles/blob/master/install.sh)"
+```
--- install.sh
+++ install.sh
@@ -1,5 +1,19 @@
-echo "Running Installer"
+echo "\n *** Running Installer  *** \n"
 
+DIRECTORY="$HOME/.dotfiles"
 
-curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
-    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+if [ -d "$DIRECTORY" ]; then
+    echo ".dotfiles already exits"
+    echo "remove it for installtion"s
+    exit
+fi
+
+mkdir $DIRECTORY
+cd $DIRECTORY
+git init
+git remote add origin  https://github.com/joey00072/dotfiles
+git pull origin master
+
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+echo "\n *** DONE *** \n"
