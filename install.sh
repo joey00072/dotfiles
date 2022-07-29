c183359c94838a171ece9b5356f3eb67caf6ca0d
Shubham Choudhari <schoudhari@platform9.com>
2022-07-29 12:57:37 +0530

    :sparkles: Added Pluginstall to installtion script

--- install.sh
+++ install.sh
@@ -26,5 +26,7 @@ ln -s $HOME/.dotfiles/.vimrc $HOME/.vimrc
 
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
+vim +'PlugInstall --sync' +qa
+
 
 echo "\n *** DONE *** \n"
