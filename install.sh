02c71238dc1ff2cf00d67c55c694d84b3c919a94
Shubham Choudhari <schoudhari@platform9.com>
2022-08-23 16:21:12 +0530

    bug fix

--- install.sh
+++ install.sh
@@ -31,7 +31,7 @@ fi
 ln -s $HOME/.dotfiles/.zshrc $HOME/.zshrc
 
 # zsh autosuggestions
-git clone https://github.com/zsh-users/zsh-autosuggestions ~/.zsh/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 
 # power level10k installation
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
